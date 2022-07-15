ed0af51a1f26f39d825055dd54695d9caa24a7cb
doubtcrack <1.edujock@gmail.com>
2022-07-15 17:08:05 +0530

    Pizza Ready Hai

--- app/models/user.js
+++ app/models/user.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, default: 'customer' }
-}, { timestamps: true })
+    password: { type: String },
+    role: { type: String, default: "customer" },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
